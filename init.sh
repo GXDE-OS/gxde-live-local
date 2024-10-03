578f40f5a29c78056b63d1a09a18130cf932cca4
gfdgd_xi <3025613752@qq.com>
2024-10-03 10:11:54 +0800

    修改路径

--- init.sh
+++ init.sh
@@ -3,7 +3,8 @@
 # 用于配置启动好的 Live 环境
 ################################
 # 卸载安装程序
-sudo apt purge calamares -y
-sudo apt purge deepin-installer -y
+apt purge calamares -y
+apt purge deepin-installer -y
 # 强制关闭进程
-sudo killall calamares deepin-installer -9
+killall calamares deepin-installer -9
+exec init
